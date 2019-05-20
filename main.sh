a7130726a8c7fe8277fc6c7eb50565d2558dedea
Vitaly Druker <vdruker@gmail.com>
2019-05-20 00:31:56 -0400

    completed dockerize of mysql db, started compose file

--- main.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-#git checkout -- .
-# git pull
-
-python run_maindef.py
--- mta_app/load_defs.py
+++ mta_app/load_defs.py
@@ -1,13 +1,21 @@
 import pandas as pd
 import sqlalchemy
-import sensative_info as si
+#import sensative_info as si
+import os
+db_user = os.environ['MYSQL_USER']
+db_pass = os.environ['MYSQL_PASSWORD']
+db_table = os.environ['MYSQL_DATABASE']
 
 
-engine_text ='mysql+mysqldb://'+si.db_user+':'+si.db_pass+'@' + si.db_host +':'+si.db_port+'/'+si.db_table
+engine_text ='mysql+mysqldb://'+db_user+':'+db_pass+'@db:3306/'+db_table
 engine = sqlalchemy.create_engine(engine_text)
+
+test_table = pd.read_sql_table(table_name = 'stops', con  = engine)
 
 stops = pd.read_csv('stops.txt')
 stops = stops[['stop_id','stop_name','stop_lat','stop_lon']]
 
-
-stops.to_sql('stops', engine, index = False, if_exists = 'append')
+try:
+    stops.to_sql('stops', engine, index = False, if_exists = 'append')
+except:
+    print('stops table already populated')
