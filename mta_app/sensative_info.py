481e5a37a5c295e963a569ea2a8e8d64142f6fe1
Vitaly Druker <vdruker@gmail.com>
2019-05-27 22:36:51 -0400

    sensative info is ok now!

---
+++ mta_app/sensative_info.py
@@ -0,0 +1,7 @@
+from os import environ 
+api_key = environ['API_KEY']
+db_host = "db" 
+db_port = "3306"
+db_user = environ['MYSQL_USER']
+db_pass = environ['MYSQL_PASSWORD']
+db_table = environ['MYSQL_DATABASE']
